0aa5eb8dbbd1f21971c1a5e09b35e982c5513d4c
santiagoTena05 <a01781293@tec.mx>
2023-04-10 13:18:05 -0600

    Node

---
+++ Desarrollo_web/back/nodeProjects/hello_Server/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/appi/hello", (req, res) => {
+    res.send("Hello World!");
+    });
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+    }
+);
+
